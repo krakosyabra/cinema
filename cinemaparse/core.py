ed5f6deedab0ee89f7514ce682fdbe2f7e35fd77
krakosyabra <57085921+krakosyabra@users.noreply.github.com>
2019-10-28 10:10:15 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,1 +1,7 @@
-
+class CinemaParser:
+    def __init__(self, city = "Moscow"):
+        if(type(city) == str):
+            self.city = city
+        else:
+            raise TypeError("Not str")
+CinemaParser(1)
