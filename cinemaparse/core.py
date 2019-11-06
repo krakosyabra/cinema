169cb3204f9566a70e9f321ccc15880cbd9066c1
krakosyabra <57085921+krakosyabra@users.noreply.github.com>
2019-11-06 09:53:16 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -6,3 +6,18 @@ class CinemaParser:
         else:
             raise TypeError("Not str")
 CinemaParser("Spb")
+
+
+
+#CinemaParser
+import requests as re
+class CinemaParser:
+    def __init__(self, city = "Msk"):
+        if isinstance(city,str) == True:
+            self.city = city
+        else:
+            raise TypeError("Not str")
+    def extract_raw_content():
+        if self.city == "Msk":
+            self.content = re.get("https://msk.subscity.ru")
+CinemaParser()
